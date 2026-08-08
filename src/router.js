17acedab8803e03f62e940f0dca507939efc1412
K <704496037@qq.com>
2026-08-08 22:03:41 +0800

    1

--- src/router.js
+++ src/router.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from "vue-router";
 import { useStore } from "@/store/store";
-import { nextTick } from "vue";
+import { defineAsyncComponent, nextTick } from "vue";
 import Home from "./home.vue";
 import setting from "./setting/setting.vue";
 import settingping from "./setting/setping.vue";
@@ -18,6 +18,13 @@ import ping from "./ping/ping.vue";
 import stms from "./st/stms.vue";
 import ec from "./st/ec.vue";
 import speedtest from "./speedtest/speedtest.vue";
+import EditCodeLoading from "./EditCode/EditCodeLoading.vue";
+
+const EditCode = defineAsyncComponent({
+  loader: () => import("./EditCode/EditCode.vue"),
+  loadingComponent: EditCodeLoading,
+  delay: 120,
+});
 
 const routes = [
   {
@@ -56,7 +63,7 @@ const routes = [
 
   {
     path: "/EditCode",
-    component: () => import("./EditCode/EditCode.vue"),
+    component: EditCode,
     name: "EditCode",
     meta: {
       title: "Edit Code",
@@ -68,7 +75,7 @@ const routes = [
 
   {
     path: "/EditCode_noNav",
-    component: () => import("./EditCode/EditCode.vue"),
+    component: EditCode,
     name: "EditCode_noNav",
     meta: {
       title: "Edit Code",
@@ -79,7 +86,7 @@ const routes = [
   },
   {
     path: "/j",
-    component: () => import("./EditCode/EditCode.vue"),
+    component: EditCode,
     name: "j",
     meta: {
       title: "Edit Code",
