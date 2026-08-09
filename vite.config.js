961a61e2f1ea4483f95b7b928c0ef35594ed4d58
K <704496037@qq.com>
2026-08-09 23:48:30 +0800

    1

--- vite.config.js
+++ vite.config.js
@@ -41,7 +41,7 @@ export default {
       workbox: {
         globPatterns: ["**/*.{js,css,html,ico,png,svg,webmanifest,json}"],
         navigateFallback: "/index.html",
-        navigateFallbackDenylist: [/^\/linkassets\//, /^\/linkjs\//, /\.js$/, /\.css$/],
+        navigateFallbackDenylist: [/^\/assets\//, /^\/.*\.(js|css|map)$/i],
         cleanupOutdatedCaches: true,
         runtimeCaching: [
           {
